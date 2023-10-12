a5bfaa888efc661f12a81c9fd1269b1f0ae348c6
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-12 16:38:07 -0700

    task3 in c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-*_islower  - is a function to check if character is lower case or not
-*@c :check input of function
+*the desciption of the function (_is lower)  - function check
+*                 if character is lower case
+* @c :chechs input of function
  *Return: returns 1 if c is lowercase, otherwise is 0
  */
 int _islower(int c)
