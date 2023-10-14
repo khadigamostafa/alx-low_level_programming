2b5e3d779044958d03c5e288d4a3a77a7793d1bb
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-14 13:42:56 -0700

    the sixth task in c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,12 @@
 
 /**
 * _abs - function that compute the absolute value of the integer
-*@int: takes integer type  input for function print_sign
+*@n: takes integer type  input for function print_sign
  *Return: return 0 (success)
  */
 int _abs(int n)
 {
-        if (n < 0)
-                n = (-1) * n;
-        return (n);
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
 }
