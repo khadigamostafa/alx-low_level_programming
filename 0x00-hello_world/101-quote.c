5523d6852231d480dd6394ed0dc51622b539de35
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-27 00:43:48 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
 #include<unistd.h>
 /*
- * main - entry point
+ * function main - the main function describes the main common in all c program
+ *                 it acts as an entry point ,it returns 1 and recieve nothing
+ *                 main - the main function
  * description :print a quote by using write function
+ * write : is a function used to print
  * @arr :as array of type char
  * return: it will return 1
  */
@@ -11,5 +14,5 @@ int main(void)
 	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(1, arr, 59);
-	return(1);
+	return (1);
 }
