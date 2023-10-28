45d5580b33f883c28bc12be30aa7a39abafa63fa
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-27 21:20:07 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,10 +5,11 @@
  * @argv: array contain the command line of the program
  * Return: it will return 0
  */
-int main(int argc, char *argv[]))
+int main(int argc, char *argv[])
 {
 	int i;
-	for (i =0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	printf("%s\n", argv[i]);
 	return (0);
 }
