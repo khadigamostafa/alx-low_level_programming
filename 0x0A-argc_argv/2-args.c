cb75510baf1c6691ecbd0e61b8e06f29e3ed9b67
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-27 21:16:59 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc : number of the argument
+ * @argv: array contain the command line of the program
+ * Return: it will return 0
+ */
+int main(int argc, char *argv[]))
+{
+	int i;
+	for (i =0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
