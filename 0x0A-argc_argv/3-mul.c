2be0519ce03660106c94510f0e4b7d076d40f69d
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-10-27 21:30:47 -0700

    task 2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc : number of the argument
+ * @argv: array contain the command line of the program
+ * Return: it will return 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1] * atoi(argv[2])));
+	return (0);
+}
